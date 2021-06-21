26d17b0021789edc74cbc472c9a66443302c5a7f
Misha Prohorenko <mishaprohorenko96@gmail.com>
2021-06-21 13:10:08 +0300

    5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 'use strict';
 
+let button = document.querySelector('#btn');
+let nameGood = document.querySelector('#nameGoods');
+let dataInp = document.querySelector('#date');
+
+button.addEventListener('click', () => {
+	console.log(nameGood.value);
+	console.log(dataInp.value)
+	dataInp.value = ''
+})
+
+
+console.log(button)
 
